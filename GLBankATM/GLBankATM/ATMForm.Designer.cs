8a716acf20292c5c3844a1a56f664548f1c670eb
Solanid <rastislav.ruscak@akademiasovy.sk>
2017-05-12 11:00:55 +0200

    ATM work progress

--- GLBankATM/GLBankATM/ATMForm.Designer.cs
+++ GLBankATM/GLBankATM/ATMForm.Designer.cs
@@ -28,19 +28,281 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.btnKeyboard1 = new System.Windows.Forms.Button();
+            this.btnKeyboard2 = new System.Windows.Forms.Button();
+            this.btnKeyboard5 = new System.Windows.Forms.Button();
+            this.btnKeyboard3 = new System.Windows.Forms.Button();
+            this.btnKeyboard4 = new System.Windows.Forms.Button();
+            this.btnKeyboard6 = new System.Windows.Forms.Button();
+            this.btnKeyboard7 = new System.Windows.Forms.Button();
+            this.btnKeyboard0 = new System.Windows.Forms.Button();
+            this.btnKeyboard8 = new System.Windows.Forms.Button();
+            this.btnKeyboard9 = new System.Windows.Forms.Button();
+            this.btnLeftPanel1 = new System.Windows.Forms.Button();
+            this.btnKeyboardC = new System.Windows.Forms.Button();
+            this.btnKeyboardOK = new System.Windows.Forms.Button();
+            this.btnLeftPanel2 = new System.Windows.Forms.Button();
+            this.btnLeftPanel3 = new System.Windows.Forms.Button();
+            this.btnLeftPanel4 = new System.Windows.Forms.Button();
+            this.btnRightPanel1 = new System.Windows.Forms.Button();
+            this.btnRightPanel2 = new System.Windows.Forms.Button();
+            this.btnRightPanel3 = new System.Windows.Forms.Button();
+            this.btnRightPanel4 = new System.Windows.Forms.Button();
+            this.panelATM = new System.Windows.Forms.Panel();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.panelATM.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // btnKeyboard1
+            // 
+            this.btnKeyboard1.Location = new System.Drawing.Point(290, 325);
+            this.btnKeyboard1.Name = "btnKeyboard1";
+            this.btnKeyboard1.Size = new System.Drawing.Size(50, 50);
+            this.btnKeyboard1.TabIndex = 0;
+            this.btnKeyboard1.Text = "1";
+            this.btnKeyboard1.UseVisualStyleBackColor = true;
+            // 
+            // btnKeyboard2
+            // 
+            this.btnKeyboard2.Location = new System.Drawing.Point(346, 325);
+            this.btnKeyboard2.Name = "btnKeyboard2";
+            this.btnKeyboard2.Size = new System.Drawing.Size(50, 50);
+            this.btnKeyboard2.TabIndex = 1;
+            this.btnKeyboard2.Text = "2";
+            this.btnKeyboard2.UseVisualStyleBackColor = true;
+            // 
+            // btnKeyboard5
+            // 
+            this.btnKeyboard5.Location = new System.Drawing.Point(346, 381);
+            this.btnKeyboard5.Name = "btnKeyboard5";
+            this.btnKeyboard5.Size = new System.Drawing.Size(50, 50);
+            this.btnKeyboard5.TabIndex = 2;
+            this.btnKeyboard5.Text = "5";
+            this.btnKeyboard5.UseVisualStyleBackColor = true;
+            // 
+            // btnKeyboard3
+            // 
+            this.btnKeyboard3.Location = new System.Drawing.Point(402, 325);
+            this.btnKeyboard3.Name = "btnKeyboard3";
+            this.btnKeyboard3.Size = new System.Drawing.Size(50, 50);
+            this.btnKeyboard3.TabIndex = 2;
+            this.btnKeyboard3.Text = "3";
+            this.btnKeyboard3.UseVisualStyleBackColor = true;
+            // 
+            // btnKeyboard4
+            // 
+            this.btnKeyboard4.Location = new System.Drawing.Point(290, 381);
+            this.btnKeyboard4.Name = "btnKeyboard4";
+            this.btnKeyboard4.Size = new System.Drawing.Size(50, 50);
+            this.btnKeyboard4.TabIndex = 3;
+            this.btnKeyboard4.Text = "4";
+            this.btnKeyboard4.UseVisualStyleBackColor = true;
+            // 
+            // btnKeyboard6
+            // 
+            this.btnKeyboard6.Location = new System.Drawing.Point(402, 381);
+            this.btnKeyboard6.Name = "btnKeyboard6";
+            this.btnKeyboard6.Size = new System.Drawing.Size(50, 50);
+            this.btnKeyboard6.TabIndex = 4;
+            this.btnKeyboard6.Text = "6";
+            this.btnKeyboard6.UseVisualStyleBackColor = true;
+            // 
+            // btnKeyboard7
+            // 
+            this.btnKeyboard7.Location = new System.Drawing.Point(290, 437);
+            this.btnKeyboard7.Name = "btnKeyboard7";
+            this.btnKeyboard7.Size = new System.Drawing.Size(50, 50);
+            this.btnKeyboard7.TabIndex = 5;
+            this.btnKeyboard7.Text = "7";
+            this.btnKeyboard7.UseVisualStyleBackColor = true;
+            // 
+            // btnKeyboard0
+            // 
+            this.btnKeyboard0.Location = new System.Drawing.Point(346, 493);
+            this.btnKeyboard0.Name = "btnKeyboard0";
+            this.btnKeyboard0.Size = new System.Drawing.Size(50, 50);
+            this.btnKeyboard0.TabIndex = 6;
+            this.btnKeyboard0.Text = "0";
+            this.btnKeyboard0.UseVisualStyleBackColor = true;
+            // 
+            // btnKeyboard8
+            // 
+            this.btnKeyboard8.Location = new System.Drawing.Point(346, 437);
+            this.btnKeyboard8.Name = "btnKeyboard8";
+            this.btnKeyboard8.Size = new System.Drawing.Size(50, 50);
+            this.btnKeyboard8.TabIndex = 6;
+            this.btnKeyboard8.Text = "8";
+            this.btnKeyboard8.UseVisualStyleBackColor = true;
+            // 
+            // btnKeyboard9
+            // 
+            this.btnKeyboard9.Location = new System.Drawing.Point(402, 437);
+            this.btnKeyboard9.Name = "btnKeyboard9";
+            this.btnKeyboard9.Size = new System.Drawing.Size(50, 50);
+            this.btnKeyboard9.TabIndex = 7;
+            this.btnKeyboard9.Text = "9";
+            this.btnKeyboard9.UseVisualStyleBackColor = true;
+            // 
+            // btnLeftPanel1
+            // 
+            this.btnLeftPanel1.Location = new System.Drawing.Point(166, 68);
+            this.btnLeftPanel1.Name = "btnLeftPanel1";
+            this.btnLeftPanel1.Size = new System.Drawing.Size(50, 50);
+            this.btnLeftPanel1.TabIndex = 8;
+            this.btnLeftPanel1.UseVisualStyleBackColor = true;
+            // 
+            // btnKeyboardC
+            // 
+            this.btnKeyboardC.Location = new System.Drawing.Point(290, 493);
+            this.btnKeyboardC.Name = "btnKeyboardC";
+            this.btnKeyboardC.Size = new System.Drawing.Size(50, 50);
+            this.btnKeyboardC.TabIndex = 8;
+            this.btnKeyboardC.Text = "C";
+            this.btnKeyboardC.UseVisualStyleBackColor = true;
+            // 
+            // btnKeyboardOK
+            // 
+            this.btnKeyboardOK.Location = new System.Drawing.Point(402, 493);
+            this.btnKeyboardOK.Name = "btnKeyboardOK";
+            this.btnKeyboardOK.Size = new System.Drawing.Size(50, 50);
+            this.btnKeyboardOK.TabIndex = 9;
+            this.btnKeyboardOK.Text = "OK";
+            this.btnKeyboardOK.UseVisualStyleBackColor = true;
+            // 
+            // btnLeftPanel2
+            // 
+            this.btnLeftPanel2.Location = new System.Drawing.Point(166, 124);
+            this.btnLeftPanel2.Name = "btnLeftPanel2";
+            this.btnLeftPanel2.Size = new System.Drawing.Size(50, 50);
+            this.btnLeftPanel2.TabIndex = 10;
+            this.btnLeftPanel2.UseVisualStyleBackColor = true;
+            // 
+            // btnLeftPanel3
+            // 
+            this.btnLeftPanel3.Location = new System.Drawing.Point(166, 180);
+            this.btnLeftPanel3.Name = "btnLeftPanel3";
+            this.btnLeftPanel3.Size = new System.Drawing.Size(50, 50);
+            this.btnLeftPanel3.TabIndex = 11;
+            this.btnLeftPanel3.UseVisualStyleBackColor = true;
+            // 
+            // btnLeftPanel4
+            // 
+            this.btnLeftPanel4.Location = new System.Drawing.Point(166, 236);
+            this.btnLeftPanel4.Name = "btnLeftPanel4";
+            this.btnLeftPanel4.Size = new System.Drawing.Size(50, 50);
+            this.btnLeftPanel4.TabIndex = 12;
+            this.btnLeftPanel4.UseVisualStyleBackColor = true;
+            // 
+            // btnRightPanel1
+            // 
+            this.btnRightPanel1.Location = new System.Drawing.Point(528, 68);
+            this.btnRightPanel1.Name = "btnRightPanel1";
+            this.btnRightPanel1.Size = new System.Drawing.Size(50, 50);
+            this.btnRightPanel1.TabIndex = 13;
+            this.btnRightPanel1.UseVisualStyleBackColor = true;
+            // 
+            // btnRightPanel2
+            // 
+            this.btnRightPanel2.Location = new System.Drawing.Point(528, 124);
+            this.btnRightPanel2.Name = "btnRightPanel2";
+            this.btnRightPanel2.Size = new System.Drawing.Size(50, 50);
+            this.btnRightPanel2.TabIndex = 14;
+            this.btnRightPanel2.UseVisualStyleBackColor = true;
+            // 
+            // btnRightPanel3
+            // 
+            this.btnRightPanel3.Location = new System.Drawing.Point(528, 180);
+            this.btnRightPanel3.Name = "btnRightPanel3";
+            this.btnRightPanel3.Size = new System.Drawing.Size(50, 50);
+            this.btnRightPanel3.TabIndex = 15;
+            this.btnRightPanel3.UseVisualStyleBackColor = true;
+            // 
+            // btnRightPanel4
+            // 
+            this.btnRightPanel4.Location = new System.Drawing.Point(528, 236);
+            this.btnRightPanel4.Name = "btnRightPanel4";
+            this.btnRightPanel4.Size = new System.Drawing.Size(50, 50);
+            this.btnRightPanel4.TabIndex = 16;
+            this.btnRightPanel4.UseVisualStyleBackColor = true;
+            // 
+            // panelATM
+            // 
+            this.panelATM.Controls.Add(this.tableLayoutPanel1);
+            this.panelATM.Location = new System.Drawing.Point(222, 57);
+            this.panelATM.Name = "panelATM";
+            this.panelATM.Size = new System.Drawing.Size(300, 250);
+            this.panelATM.TabIndex = 17;
+            this.panelATM.Paint += new System.Windows.Forms.PaintEventHandler(this.panelATM_Paint);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(200, 100);
+            this.tableLayoutPanel1.TabIndex = 0;
             // 
             // ATMForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(731, 647);
+            this.Controls.Add(this.panelATM);
+            this.Controls.Add(this.btnRightPanel4);
+            this.Controls.Add(this.btnRightPanel3);
+            this.Controls.Add(this.btnRightPanel2);
+            this.Controls.Add(this.btnRightPanel1);
+            this.Controls.Add(this.btnLeftPanel4);
+            this.Controls.Add(this.btnLeftPanel3);
+            this.Controls.Add(this.btnLeftPanel2);
+            this.Controls.Add(this.btnKeyboardOK);
+            this.Controls.Add(this.btnKeyboardC);
+            this.Controls.Add(this.btnLeftPanel1);
+            this.Controls.Add(this.btnKeyboard9);
+            this.Controls.Add(this.btnKeyboard8);
+            this.Controls.Add(this.btnKeyboard0);
+            this.Controls.Add(this.btnKeyboard7);
+            this.Controls.Add(this.btnKeyboard6);
+            this.Controls.Add(this.btnKeyboard4);
+            this.Controls.Add(this.btnKeyboard3);
+            this.Controls.Add(this.btnKeyboard5);
+            this.Controls.Add(this.btnKeyboard2);
+            this.Controls.Add(this.btnKeyboard1);
             this.Name = "ATMForm";
             this.Text = "ATMForm";
+            this.panelATM.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnKeyboard1;
+        private System.Windows.Forms.Button btnKeyboard2;
+        private System.Windows.Forms.Button btnKeyboard5;
+        private System.Windows.Forms.Button btnKeyboard3;
+        private System.Windows.Forms.Button btnKeyboard4;
+        private System.Windows.Forms.Button btnKeyboard6;
+        private System.Windows.Forms.Button btnKeyboard7;
+        private System.Windows.Forms.Button btnKeyboard0;
+        private System.Windows.Forms.Button btnKeyboard8;
+        private System.Windows.Forms.Button btnKeyboard9;
+        private System.Windows.Forms.Button btnLeftPanel1;
+        private System.Windows.Forms.Button btnKeyboardC;
+        private System.Windows.Forms.Button btnKeyboardOK;
+        private System.Windows.Forms.Button btnLeftPanel2;
+        private System.Windows.Forms.Button btnLeftPanel3;
+        private System.Windows.Forms.Button btnLeftPanel4;
+        private System.Windows.Forms.Button btnRightPanel1;
+        private System.Windows.Forms.Button btnRightPanel2;
+        private System.Windows.Forms.Button btnRightPanel3;
+        private System.Windows.Forms.Button btnRightPanel4;
+        private System.Windows.Forms.Panel panelATM;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
     }
 }
--- GLBankATM/GLBankATM/ATMForm.cs
+++ GLBankATM/GLBankATM/ATMForm.cs
@@ -10,11 +10,47 @@ using System.Windows.Forms;
 
 namespace GLBankATM
 {
+    public enum States
+    {
+        LANGUAGE, ENTERPIN, PINOK, PINWRONG, INVALIDCARD
+    }
+    public enum Languages
+    {
+        SVK, ENG
+    }
+
     public partial class ATMForm : Form
     {
         public ATMForm()
         {
             InitializeComponent();
+        }
+
+        private void panelATM_Paint(object sender, PaintEventArgs e)
+        {
+            States state = new States();
+            switch (state)
+            {
+                case States.LANGUAGE:
+                    break;
+                case States.ENTERPIN:
+                    break;
+                case States.PINOK:
+                    break;
+                case States.PINWRONG:
+                    break;
+                case States.INVALIDCARD:
+                    break;
+                default:
+                    break;
+            }
+            if (state == States.ENTERPIN)
+            {
+                if (true)
+                {
+
+                }
+            }
         }
     }
 }
